b17ef37ec552c55002895a90dbc1ae7c9932008a
onryl <onrylmz@live.com>
2022-06-03 16:28:28 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+        public static void main(String[] args) throws InterruptedException {
+
+            System.setProperty("webdriver.chrome.driver",
+                    "C:\\Users\\mirackasap\\Desktop\\Selenium\\executables\\chromedriver.exe");
+            ChromeDriver driver = new ChromeDriver();
+            driver.get("https://gobito.akademi.ws/page/90/login");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            // Login
+            driver.findElement(By.name("username")).sendKeys("onur");
+            driver.findElement(By.name("password")).sendKeys("Onr_090807");
+
+            driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+            // QservExam Ekleme
+            driver.findElement(By.xpath("//img[@class='sc-fHuLdG jbkWec']")).click();
+            driver.findElement(By.xpath("//span[@class='MuiButton-startIcon MuiButton-iconSizeSmall']")).click();
+            driver.findElement(By.xpath("//p[@class='sc-dwfUOf sc-dkIXFM irUTVA gioTxt']")).click();
+            driver.findElement(By.xpath("//img[@src='/page/90/assets/img/providers/QServExam.png']")).click();
+
+        }
+
+    }
+
+
